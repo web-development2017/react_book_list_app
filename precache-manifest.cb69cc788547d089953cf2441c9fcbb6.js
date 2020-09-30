c306f1c7818d03195b2bf864766f2520e3fc2f67
Rich <web_dev2017@outlook.com>
2020-09-30 13:52:14 +0100

    Updates

---
+++ precache-manifest.cb69cc788547d089953cf2441c9fcbb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a7b5ff0a6a8eb522196e6207a2c363b",
+    "url": "/react_book_list_app/index.html"
+  },
+  {
+    "revision": "ab0866002689b1b6c05b",
+    "url": "/react_book_list_app/static/css/main.f7e1efb5.chunk.css"
+  },
+  {
+    "revision": "a1803d47a8be93d1101d",
+    "url": "/react_book_list_app/static/js/2.205be5fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_book_list_app/static/js/2.205be5fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab0866002689b1b6c05b",
+    "url": "/react_book_list_app/static/js/main.2773cff2.chunk.js"
+  },
+  {
+    "revision": "a319dd224b7933c58da7",
+    "url": "/react_book_list_app/static/js/runtime-main.e67fa794.js"
+  }
+]);
